e7aa951fc0c824129e1c2d55729cf4ab4bc30c1c
Super <Super@Maks_M16>
2024-01-19 20:57:57 -0500

    Homework 1

---
+++ LSP_2024/src/org/howard/edu/lsp/assignment1/Hello_World.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.assignment1;
+/**
+ * Makuochukwu Onwubiko
+ */
+public class Hello_World {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
